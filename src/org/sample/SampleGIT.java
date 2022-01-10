f1440967a0ef2f51290b428de03ae06ff1b1d4c0
Dinesh1705 <dine00sh@gmail.com>
2022-01-10 09:01:54 +0530

    done

--- src/org/sample/SampleGIT.java
+++ src/org/sample/SampleGIT.java
@@ -1,5 +1,13 @@
 package org.sample;
 
 public class SampleGIT {
+	private void push() {
+		System.out.println("Sample Push Code");
 
+	}
+
+	public static void main(String[] args) {
+		SampleGIT s=new SampleGIT();
+		s.push();
+	}
 }
